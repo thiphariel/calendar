12c85763028e5a63a95817438904b81acb349d01
Thiphariel <thiphariel@gmail.com>
2020-07-28 22:15:18 +0200

    Updates

---
+++ precache-manifest.63acec9cb03dbe8c38c48d1f829e78de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4248eda4dc04c1c3aef4e067d7fe6dd6",
+    "url": "/calendar/index.html"
+  },
+  {
+    "revision": "69b820e02b35206a3ddf",
+    "url": "/calendar/static/css/2.b6ddee0d.chunk.css"
+  },
+  {
+    "revision": "d7735b700e8c07d9c040",
+    "url": "/calendar/static/css/main.6349c00f.chunk.css"
+  },
+  {
+    "revision": "69b820e02b35206a3ddf",
+    "url": "/calendar/static/js/2.640cdc8e.chunk.js"
+  },
+  {
+    "revision": "2472e0a3c4bba598b73516ac7bc920f8",
+    "url": "/calendar/static/js/2.640cdc8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7735b700e8c07d9c040",
+    "url": "/calendar/static/js/main.5f539c93.chunk.js"
+  },
+  {
+    "revision": "7d0fb5c2975c2066ee7b",
+    "url": "/calendar/static/js/runtime-main.914f0415.js"
+  }
+]);
